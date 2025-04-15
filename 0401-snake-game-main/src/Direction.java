46f92a8e9cd117dbd65c67909058d661b597db22
cassandrawolfe <clw192@pitt.edu>
2025-04-15 10:17:05 -0400

    direction enum

--- 0401-snake-game-main/src/Direction.java
+++ 0401-snake-game-main/src/Direction.java
@@ -6,11 +6,18 @@
 
 public enum Direction {
     // TODO: Implement.
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
     ;
 
     public Position deltaPosition() {
         // TODO: should return the new position after the snake has moved
         // The position should either move +1 or -1 along the x or y axis
+        switch (direction) {
+            case: 
+        }
         return new Position(0, 0); // default position if not moving
     }
 }
