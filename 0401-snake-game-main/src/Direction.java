bdc7bed335edd82e307c79f5d93760c0555e54cc
fah73 <fah73@pitt.edu>
2025-04-15 10:12:23 -0400

    first commit

---
+++ 0401-snake-game-main/src/Direction.java
@@ -0,0 +1,16 @@
+/*
+ * Created on 2025-04-03
+ *
+ * Copyright (c) 2025 Nadine von Frankenberg
+ */
+
+public enum Direction {
+    // TODO: Implement.
+    ;
+
+    public Position deltaPosition() {
+        // TODO: should return the new position after the snake has moved
+        // The position should either move +1 or -1 along the x or y axis
+        return new Position(0, 0); // default position if not moving
+    }
+}
