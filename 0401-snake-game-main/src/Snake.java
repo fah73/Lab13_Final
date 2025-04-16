b403d32f8ca9c7e825d4d19543f05d26ed908a5e
cassandrawolfe <clw192@pitt.edu>
2025-04-16 16:24:19 -0400

    lots of changes to snake

--- 0401-snake-game-main/src/Snake.java
+++ 0401-snake-game-main/src/Snake.java
@@ -10,7 +10,9 @@ import java.util.LinkedList;
 public class Snake {
     private SnakeSegment head;
     private Direction direction = Direction.RIGHT; // Default direction
-    LinkedList<SnakeSegment> snake = new LinkedList<SnakeSegment>(); //Creates a linked list to make a snake
+    //TODO test
+    private boolean shouldGrow = false;
+    LinkedList<SnakeSegment> snake = new LinkedList<>(); //Creates a linked list to make a snake
 
 
 
@@ -19,15 +21,30 @@ public class Snake {
         // Feel free to make the starting position random
         Position startingPosition = new Position(10, 10);
         head = new SnakeSegment(startingPosition);
+        snake.add(head);
     }
 
     // TODO: The snake should grow whenever it "eats" a food item
     public void shouldGrow() {
-
+        //snake.addLast(body);
+        shouldGrow = true;
     }
 
     // TODO: Remove the last node (tail) of the snake, leave head untouched
     private void removeTail() {
+        if (snake.size() > 1) {
+            snake.removeLast();//Removes the last from the LinkedList
+
+            //Updates the linked list of SnakeSegments
+            SnakeSegment current = head;
+            while (current.getNext() != null && current.getNext().getNext() != null) {
+                current = current.getNext();
+            }
+
+            if (current != null) {
+                current.setNext(null); //Cuts off the old tail
+            }
+        }
 
     }
 
@@ -42,6 +59,12 @@ public class Snake {
     // TODO: Implement isInSnake()
     // Returns false if the specified position is inside the body of the snake
     public boolean isBodyPartAt(Position position) {
+        //Head is index 0, skips head
+        for (int i = 1; i < snake.size(); i++) {
+            if (snake.get(i).getPosition().equals(position)) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -54,7 +77,11 @@ public class Snake {
     // TODO: Get the length of the snake
     public int getLength() {
         int count = 0;
-
+        SnakeSegment current = head;
+        while (current!= null) {
+            count++;
+            current = current.getNext();
+        }
         return count;
     }
 
@@ -62,7 +89,13 @@ public class Snake {
     // TODO: Implement move()
     public void move() {
         Position newPosition = head.getPosition().add(direction.deltaPosition());
+        SnakeSegment newHead = new SnakeSegment(newPosition);
 
+        newHead.setNext(head); //Links new head to old head
+        head = newHead;
+        snake.addFirst(newHead); //Adds to the front of the linked list tracker
+
+        //TODO REMOVE IF WORKS
         // ...
 
         // * OPTIONAL: also handle wrapping left and right here
@@ -85,6 +118,9 @@ public class Snake {
 
     // TODO: Return the start of the body (NOT the head!)
     public SnakeSegment getBody() {
+        if (snake.size() > 1) {
+            return snake.get(1); //Index 0 is the head
+        }
         return null;
     }
 
